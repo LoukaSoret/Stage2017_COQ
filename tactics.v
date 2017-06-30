cf83bcd68248517d0b647df6c8429a134f371b50
Louka Soret <LoukaSoret@users.noreply.github.com>
2017-06-30 09:19:03 +0200

    fix du pattern matching sur les hypotheses + try

--- tactics.v
+++ tactics.v
@@ -1,56 +1,59 @@
 Ltac implique_intro h := 
 	match goal with
-	| |- (?X1 -> ?X2) => intro h
+	| |- (?X1 -> ?X2) => try intro h
+	| |- (~?X1) => try intro h
 	| _ => idtac "Erreur: aucun implique dans le but courant."
 	end
 .
 
+Ltac noname h i j := try pose (j := h i).
+
 Ltac implique_elim h :=
 	match goal with
-	| |- (?X1 -> ?X2) => elim h
-	| |- (?X1) => apply h
+	| |- (?X1 -> ?X2) => try elim h
+	| |- (?X1) => try apply h
 	| _ => idtac "Erreur: l'hypotese n'est pas appliquable au but courant."
 	end
 .
 
 Ltac et_elim_1 h h_decomp :=
-	match h with
-	| (?X1 /\ ?X2) => elim h; intros h_decomp Tmp ; clear Tmp
+	match type of h with
+	| _ /\ _ => try ( elim h; intros h_decomp Tmp ; clear Tmp ) 
 	| _ => idtac "Erreur: l'hypotese n'est pas decomposable."
 	end
 .
 
 Ltac et_elim_2 h h_decomp :=
-	match h with
-	| (?X1 /\ ?X2) => elim h; intros Tmp h_decomp ; clear Tmp
+	match type of h with
+	| _ /\ _ => try (elim h; intros Tmp h_decomp ; clear Tmp)
 	| _ => idtac "Erreur: l'hypotese n'est pas decomposable."
 	end
 .
 
 Ltac et_intro :=
 	match goal with
-	| |- (?X1 /\ ?X2) => split
+	| |- (?X1 /\ ?X2) => try split
 	| _ => idtac "Erreur: le but courant n'est pas décomposable."
 	end
 .
 
 Ltac ou_intro_1 :=
 	match goal with
-	| |- (?X1 \/ ?X2) => left
+	| |- (?X1 \/ ?X2) => try left
 	| _ => idtac "Erreur: introduction de l'hypotese impossible."
 	end
 .
 
 Ltac ou_intro_2 :=
 	match goal with
-	| |- (?X1 \/ ?X2) => right
+	| |- (?X1 \/ ?X2) => try right
 	| _ => idtac "Erreur: introduction de l'hypotese impossible."
 	end
 .
 
 Ltac ou_elim h :=
-	match h with
-	| (?X1 \/ ?X2) => elim h
+	match type of h with
+	| _ \/ _ => try elim h
 	| _ => idtac "Erreur: forme de l'hypotese incorrecte."
 	end
 .
@@ -59,8 +62,8 @@ Ltac efq := exfalso.
 
 Ltac raa :=
 	match goal with
-	| |- (~~?X1) => intro tmp ; apply tmp ; clear tmp
-	| |- (?X1 -> False -> False) => intro tmp ; apply tmp ; clear tmp
+	| |- (~~?X1) => try (intro tmp ; apply tmp ; clear tmp)
+	| |- (?X1 -> False -> False) => try (intro tmp ; apply tmp ; clear tmp)
 	| _ => idtac "Erreur: reduction a l absurde impossible sur le but courant."
 	end
 .
